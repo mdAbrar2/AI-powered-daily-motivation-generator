7fe07b59d31891da68e0f57d63c9e610b0e3b19a
Abrar <mdabrar.dev@gmail.com>
2025-05-16 17:44:13 +0530

    commiting wsgi files changes

--- motivation_project/motivation_project/wsgi.py
+++ motivation_project/motivation_project/wsgi.py
@@ -18,7 +18,8 @@ if project_home not in sys.path:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'motivation_project.settings'
 
 # Activate your virtualenv if you have one
-activate_this = '/home/yourusername/motivation project/venv/bin/activate_this.py'
+activate_this = '/home/mdabrardev/AI-powered-daily-motivation-generator/venv/bin/activate_this.py'
+# /home/mdabrardev/AI-powered-daily-motivation-generator/venv
 if os.path.exists(activate_this):
     with open(activate_this) as file_:
         exec(file_.read(), dict(__file__=activate_this))
