d8c5059f5a1d6d288dc033d9195eb29780656720
Abrar <mdabrar.dev@gmail.com>
2025-05-16 17:01:35 +0530

    again trying to do some setup for collecting static files

--- motivation_project/motivation_project/settings.py
+++ motivation_project/motivation_project/settings.py
@@ -119,14 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+# For production: where collectstatic will put all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+# For development: where Django will look for extra static files
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
